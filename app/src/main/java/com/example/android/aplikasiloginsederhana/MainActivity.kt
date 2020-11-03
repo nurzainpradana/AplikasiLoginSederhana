1f49d8c6a27dbb34b077a41de4df99dd47abfa22
nurzainpradana <pradananurzain@gmail.com>
2020-11-03 14:15:11 +0700

    initial commit

---
+++ app/src/main/java/com/example/android/aplikasiloginsederhana/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.android.aplikasiloginsederhana
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun verifikasi(view: View) {
+        var username = ""
+        var password = ""
+
+        // Mengecek Username kosong atau tidak
+        if (et_username.text.toString() != ""){
+            username = et_username.text.toString()
+        } else {
+            et_username.setError("Wajib Diisi")
+        }
+
+        // Mengecek Password kosong atau tidak
+        if (et_password.text.toString() != ""){
+            password = et_password.text.toString()
+        } else {
+            et_password.setError("Wajib Diisi")
+        }
+
+        // Proses verifikasi
+        if (username != "" || password != ""){
+            Toast.makeText(this, "TEST", Toast.LENGTH_SHORT)
+        }
+    }
+}
