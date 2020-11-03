c024178a2e46147440d191ab8960458ac5745cda
nurzainpradana <pradananurzain@gmail.com>
2020-11-03 14:44:45 +0700

    restore layout main

--- app/src/main/java/com/example/android/aplikasiloginsederhana/MainActivity.kt
+++ app/src/main/java/com/example/android/aplikasiloginsederhana/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.android.aplikasiloginsederhana
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var username: String
+    lateinit var password: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,6 +19,24 @@ class MainActivity : AppCompatActivity() {
     fun verifikasi(view: View) {
         var username = ""
         var password = ""
+
+        if (checkInput(et_username) && checkInput(et_password)){
+            username = et_username.text.toString()
+            password = et_password.text.toString()
+
+            var dummyUsername = "zain"
+            var dummyPassword = "zain123"
+
+            if (username.equals(dummyUsername)){
+                if (password.equals(dummyPassword)){
+                    Toast.makeText(this, "Login Berhasil", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Password anda Salah", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Username anda tidak terdaftar", Toast.LENGTH_SHORT).show()
+            }
+        }
 
         // Mengecek Username kosong atau tidak
         if (et_username.text.toString() != ""){
@@ -43,6 +65,15 @@ class MainActivity : AppCompatActivity() {
             } else {
                 Toast.makeText(this, "Username anda tidak terdaftar", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
+
+    private fun checkInput(et: EditText) : Boolean {
+        return if (et.text.toString() != ""){
+            true
+        } else {
+            et.setError("Wajib Diisi")
+            false
         }
     }
 }
